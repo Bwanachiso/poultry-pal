6da8c5b78f420a026d68369e347de3cd6e61f00e
tamani phiri <tamanigabriel0@gmail.com>
2023-10-31 09:09:10 +0200

    changes

--- client/src/components/ChatComponent.tsx
+++ client/src/components/ChatComponent.tsx
@@ -1,5 +1,5 @@
 // ChatComponent.tsx
-import React, { useState, useEffect } from 'react';
+import{ useState, useEffect } from 'react';
 import axios from 'axios';
 
 const ChatComponent: React.FC = () => {
